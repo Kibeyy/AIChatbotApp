4dceffecb83c23955bd80731bab2c08768270753
Kibeyy <collinskibe6477@gmail.com>
2025-04-27 23:29:58 +0300

    Learning about room

---
+++ Projects/roomdb_lesson/app/build.gradle.kts
@@ -0,0 +1,81 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.example.roomdb_lesson"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.roomdb_lesson"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Versions
+    val room_version = "2.6.1"
+    val hilt_version = "2.51"
+
+    dependencies {
+        // Room (Database)
+        implementation(libs.androidx.room.runtime)
+        kapt(libs.androidx.room.compiler)
+
+        // Optional - Room Kotlin Extensions and Coroutines support
+        implementation(libs.androidx.room.ktx)
+
+        // Optional - Room Paging 3 Integration (only if you use Paging)
+        implementation(libs.androidx.room.paging)
+
+        // Dagger Hilt (Dependency Injection)
+        implementation(libs.hilt.android)
+        kapt(libs.hilt.android.compiler)
+    }
+}
---
+++ Projects/roomdb_lesson/app/src/main/java/com/example/roomdb_lesson/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.example.roomdb_lesson
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.roomdb_lesson.ui.theme.Roomdb_lessonTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MainScreen(){
+    val input = remember {
+        mutableStateOf("")
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        MaterialTheme {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(20.dp)
+            ) {
+               Row (
+                   verticalAlignment = Alignment.CenterVertically,
+                   horizontalArrangement = Arrangement.SpaceBetween,
+                   modifier = Modifier
+                       .fillMaxWidth()
+                       .height(60.dp)
+
+                       //.weight(1f)
+               ){
+                   OutlinedTextField(
+                       value = input.value,
+                       label = {Text(text = "Name")},
+                       onValueChange = {input.value = it},
+                       modifier = Modifier
+                           .width(200.dp)
+
+
+                   )
+                   Button(
+                       onClick = {},
+                       shape = RoundedCornerShape(10.dp),
+                       modifier = Modifier
+                           .padding(top = 5.dp)
+                           .fillMaxHeight()
+                   ) {
+                       Text(text = "ADD NAME")
+                   }
+               }
+
+
+
+            }
+        }
+    }
+}
+
