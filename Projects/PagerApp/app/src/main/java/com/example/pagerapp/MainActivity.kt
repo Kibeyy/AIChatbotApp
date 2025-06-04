61635f30a4848d5f6cff1afb84c5c6c82d9202e1
Kibeyy <collinskibe6477@gmail.com>
2025-06-04 15:03:50 +0300

    Made a pager app using compose

---
+++ Projects/PagerApp/app/src/main/java/com/example/pagerapp/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.pagerapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.pagerapp.ui.theme.PagerAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PagerAppTheme {
+                Home()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun Home(){
+    val pages = listOf(
+        R.drawable.slideone,
+        R.drawable.slidetwo,
+        R.drawable.slidethree
+    )
+    val pagerState = rememberPagerState(initialPage = 0, pageCount = {pages.size})
+    Scaffold(
+        topBar = {
+            TopAppBar(
+
+                title = {Text(text = "Pager App", fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic, letterSpacing = 2.sp)},
+
+                )
+        }
+    ) { paddingValues ->
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+        ) {
+            HorizontalPager(pagerState, modifier = Modifier.fillMaxSize()) { index ->
+                Image(painter = painterResource(pages[index]),
+                    modifier = Modifier.fillMaxSize(),
+                    contentDescription = null, contentScale = ContentScale.Crop)
+
+
+            }
+
+        }
+
+    }
+}
